c2697f428dea6a2f0fbf9428b172504f68df3e87
gcgvgvvfsvsfvgmailcom <119113162+OgFishiesYT@users.noreply.github.com>
2023-05-06 12:37:34 +0100

    new api

--- RecRoom2020/CommunityBoard-api-get.py
+++ RecRoom2020/CommunityBoard-api-get.py
@@ -1,20 +1,17 @@
-
-#The API is not being ues rn
-
-
-
-from django.http import HttpResponse
-from django.shortcuts import render 
-
 import requests
 
 api = requests.get("https://ogfishiesyt.github.io/RR2020.github.io/CommunityBoard.json")
 
 rec_net = ('https://cdn.rec.net/video/')
 
-BlobName0 = api.json()['Videos'][0]['BlobName']
-Title1 = api.json()['Videos'][0]["Title"]
+no = int(input('no. of video'))
+print (no)
 
-def video1(requset):
-    return render(requests, "index.html",{"q": rec_net+BlobName0})
+BlobName0 = api.json()['Videos'][no]['BlobName']
+Title0 = api.json()['Videos'][no]["Title"]
+
+
+
+print(rec_net+BlobName0)
+print(Title0)
 
