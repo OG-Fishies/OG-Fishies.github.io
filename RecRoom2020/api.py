defbfb6331aed13293cf04ef804aed8281a86d6c
gcgvgvvfsvsfvgmailcom <119113162+OgFishiesYT@users.noreply.github.com>
2023-05-08 13:09:10 +0100

    thx

--- RecRoom2020/api.py
+++ RecRoom2020/api.py
@@ -1,7 +1,16 @@
+
+#this was a test
+
+
+
+
 import requests
 
 API = requests.get('https://ogfishiesyt.github.io/RR2020.github.io/CommunityBoard.json')
 
+#CurrentAnnouncement
+Announcement = API.json()['CurrentAnnouncement']['Message']
+MoreInfoUrl = API.json()['CurrentAnnouncement']['MoreInfoUrl']
 #videos
 video1 = API.json()['Videos'][0]['BlobName']
 video2 = API.json()['Videos'][1]['BlobName']
@@ -10,3 +19,15 @@ video3 = API.json()['Videos'][2]['BlobName']
 Title1 = API.json()['Videos'][0]['Title']
 Title2 = API.json()['Videos'][1]['Title']
 Title3 = API.json()['Videos'][2]['Title']
+#testing
+print(Announcement)
+print(MoreInfoUrl)
+print()
+print(video1)
+print(Title1)
+print()
+print(video2)
+print(Title2)
+print()
+print(video3)
+print(Title3)
